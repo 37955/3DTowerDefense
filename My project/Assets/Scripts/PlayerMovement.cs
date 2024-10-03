2d24e69e9952190789c09a6fe3e357af7a485a9f
Pieter van der Schoot <37955@ma-web.nl>
2024-10-03 17:39:35 +0200

    Player Jump

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speed = 2f;
-
+    bool canJump = true;
+    float jumpForce = 5f;
+    [SerializeField]
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,9 +24,32 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         //update the position
-        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime,0 , verticalInput * speed * Time.deltaTime);
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
 
         //output to log the position change
-        Debug.Log(transform.position);
+
+        //if raycast = true
+        // canjump = true
+
+
+    }
+    private void FixedUpdate()
+    {
+        int layerMask = 1 << 3;
+        layerMask = ~layerMask;
+        if (Input.GetButton("Jump") && canJump)
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+            canJump = false;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.5f, layerMask))
+        {
+            Debug.Log(hit.transform.gameObject.layer);
+            canJump = true;
+
+        }
     }
 }
