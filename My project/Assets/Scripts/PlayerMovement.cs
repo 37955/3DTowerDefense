fc433fbf87ba5952b86e12df286609af8f77ac76
Pieter van der Schoot <37955@ma-web.nl>
2024-10-03 16:41:38 +0200

    Player Movement

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime,0 , verticalInput * speed * Time.deltaTime);
+
+        //output to log the position change
+        Debug.Log(transform.position);
     }
 }
